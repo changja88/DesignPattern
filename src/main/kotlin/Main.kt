aefbbce96b12b19ffcdb03b8350571900c95cbbf
hyun <hyun@hyunui-Mac-Studio.local>
2023-01-04 15:13:28 +0900

    DDD Chapter 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,3 +8,18 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
+
+class Caculater2 constructor(initialNum: Int = 0) {
+    var result: Int = 10
+        set(value) {
+            field = value
+        }
+
+    init {
+        this.result = initialNum
+    }
+    fun calculater(operater:Char, num: Int){
+
+    }
+}
